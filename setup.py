8e4815e17fb13d7240ba64876cf18024c114b0c5
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2025-03-10 12:56:20 +0530

    Refactor

--- setup.py
+++ setup.py
@@ -1,7 +1,28 @@
 
-
+from os import path
+import re
 from setuptools import setup, find_packages
+def read_requirements(path):
+    with open(path, "r") as f:
+        requirements = f.read().splitlines()
+        processed_requirements = []
 
+        for req in requirements:
+            # For git or other VCS links
+            if req.startswith("git+") or "@" in req:
+                pkg_name = re.search(r"(#egg=)([\w\-_]+)", req)
+                if pkg_name:
+                    processed_requirements.append(pkg_name.group(2))
+                else:
+                    # You may decide to raise an exception here,
+                    # if you want to ensure every VCS link has an #egg=<package_name> at the end
+                    continue
+            else:
+                processed_requirements.append(req)
+        return processed_requirements
+
+
+requirements = read_requirements("requirements.txt")
 setup(
     name='bittensor-subnet',
     version='0.1.0',
@@ -10,16 +31,7 @@ setup(
     description='A Bittensor subnet for audio captioning and transcription evaluation.',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
-    install_requires=[
-        'bittensor',
-        'numpy',
-        'scipy',
-        'pydub',
-        'torch',  # Assuming you're using PyTorch for STT models
-        'transformers',  # If using Hugging Face models like Whisper
-        'soundfile',  # For audio file handling
-        'webrtcvad',  # For voice activity detection
-    ],
+    install_requires=requirements,
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
